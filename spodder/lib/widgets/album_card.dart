99afda18b4aa4256a8b38ae1b7fbec9c72e3b7e8
Tanish Khandelwal <76485783+tknishh@users.noreply.github.com>
2022-05-18 06:37:31 +0530

    completed

---
+++ spodder/lib/widgets/album_card.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:spodder/views/album_view.dart';
+
+class AlbumCard extends StatelessWidget {
+  final ImageProvider image;
+  final String label;
+  final Function onTap;
+  final double size;
+  const AlbumCard({
+    Key? key,
+    required this.image,
+    required this.label,
+    required this.onTap,
+    this.size = 120,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              var key2 = null;
+              return AlbumView(
+                image: image,
+                key: key2,
+              );
+            },
+          ),
+        );
+      },
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image(
+            image: image,
+            width: size,
+            height: size,
+            fit: BoxFit.cover,
+          ),
+          SizedBox(height: 10),
+          Text(label),
+        ],
+      ),
+    );
+  }
+}
---
+++ spodder/lib/widgets/song_card.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:spodder/views/album_view.dart';
+
+class SongCard extends StatelessWidget {
+  final AssetImage image;
+
+  const SongCard({
+    Key? key,
+    required this.image,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              var key2 = null;
+              return AlbumView(
+                image: image,
+                key: key2,
+              );
+            },
+          ),
+        );
+      },
+      child: Container(
+        width: 140,
+        child: Column(
+          children: [
+            Image(
+              image: image,
+              width: 140,
+              height: 140,
+            ),
+            Text(
+              "Bad Bunny, Drake, Justin Bieber, Taylor Swift, Ed Sheeran",
+              style: Theme.of(context).textTheme.caption,
+              overflow: TextOverflow.ellipsis,
+              maxLines: 2,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
